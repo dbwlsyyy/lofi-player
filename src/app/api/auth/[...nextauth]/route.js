f35755ddcc6cc3b37347f6d12add4333020ed1c2
홍유진 <dbwlsyyy@naver.com>
2025-11-02 18:24:03 +0900

    feat: 로그인 화면

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,14 @@
+import NextAuth from 'next-auth';
+import SpotifyProvider from 'next-auth/providers/spotify';
+
+const handler = NextAuth({
+    providers: [
+        SpotifyProvider({
+            clientId: process.env.SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+            authorization: { params: { scope: process.env.SPOTIFY_SCOPE } },
+        }),
+    ],
+});
+
+export { handler as GET, handler as POST };
