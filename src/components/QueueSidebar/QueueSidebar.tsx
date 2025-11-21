59ebc2dbaaad5fc17f710877d6a20c0ed49aa400
홍유진 <dbwlsyyy@naver.com>
2025-11-22 01:25:38 +0900

    design: QueueSidebar 디자인 수정

--- src/components/QueueSidebar/QueueSidebar.tsx
+++ src/components/QueueSidebar/QueueSidebar.tsx
@@ -1,28 +1,73 @@
 'use client';
-
 import { usePlayerStore } from '@/store/usePlayerStore';
 import styles from './QueueSidebar.module.css';
 
 export default function QueueSidebar() {
-    const { queue, currentTrack, isQueueOpen } = usePlayerStore();
+    const { queue, currentIndex, currentTrack, isQueueOpen } = usePlayerStore();
 
     return (
         <aside
             className={`${styles.sidebar} ${isQueueOpen ? styles.open : ''}`}
         >
-            <h2 className={styles.title}>재생목록</h2>
+            <h2 className={styles.title}>Playlist</h2>
+            {currentTrack && (
+                <>
+                    <div className={styles.sectionTitle}>지금 재생 중</div>
+
+                    <div className={`${styles.item} ${styles.active}`}>
+                        <img
+                            src={currentTrack.image}
+                            className={styles.thumb}
+                        />
+
+                        <div className={styles.textGroup}>
+                            <div className={styles.titleText}>
+                                {currentTrack.name}
+                            </div>
+                            <div className={styles.artistText}>
+                                {currentTrack.artists.join(', ')}
+                            </div>
+                        </div>
+
+                        <div className={styles.eqWrapper}>
+                            <div className={styles.eqBar}></div>
+                            <div className={styles.eqBar}></div>
+                            <div className={styles.eqBar}></div>
+                        </div>
+                    </div>
+                </>
+            )}
+            <div className={styles.sectionTitle}>현재 재생목록</div>
 
             <div className={styles.list}>
-                {queue.map((track) => (
-                    <div
-                        key={track.id}
-                        className={`${styles.item} ${
-                            track.id === currentTrack?.id ? styles.active : ''
-                        }`}
-                    >
-                        {track.name}
-                    </div>
-                ))}
+                {queue.map((track, index) => {
+                    const isActive = index === currentIndex;
+
+                    return (
+                        <div
+                            key={track.id}
+                            className={`${styles.item} ${isActive ? styles.activeBlack : ''}`}
+                        >
+                            <img src={track.image} className={styles.thumb} />
+
+                            <div className={styles.textGroup}>
+                                <div className={styles.titleText}>
+                                    {track.name}
+                                </div>
+                                <div className={styles.artistText}>
+                                    {track.artists.join(', ')}
+                                </div>
+                            </div>
+                            {isActive && (
+                                <div className={styles.eqWrapper}>
+                                    <div className={styles.eqBarBlack}></div>
+                                    <div className={styles.eqBarBlack}></div>
+                                    <div className={styles.eqBarBlack}></div>
+                                </div>
+                            )}
+                        </div>
+                    );
+                })}
             </div>
         </aside>
     );
