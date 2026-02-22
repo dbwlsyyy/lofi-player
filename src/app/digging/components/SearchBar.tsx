d7946e85ea57ab25603ebbc897db99bbd50bf7ba
홍유진 <dbwlsyyy@naver.com>
2026-02-22 18:12:54 +0900

    refactor: 컴포넌트 분리

---
+++ src/app/digging/components/SearchBar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { FiSearch } from "react-icons/fi";
+import styles from "../Digging.module.css";
+
+interface SearchBarProps {
+  query: string;
+  setQuery: (q: string) => void;
+  placeholder: string;
+}
+
+export default function SearchBar({ query, setQuery, placeholder }: SearchBarProps) {
+  return (
+    <div className={styles.searchHeader}>
+      <div className={styles.searchBoxMinimal}>
+        <FiSearch className={styles.searchIcon} />
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className={styles.searchInput}
+        />
+      </div>
+    </div>
+  );
+}
