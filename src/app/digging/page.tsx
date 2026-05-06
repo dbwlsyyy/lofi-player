c56525574325092f39c2f22444618c1404f0eae4
홍유진 <dbwlsyyy@naver.com>
2026-05-06 15:46:25 +0900

    fix: 앨범 데이터 중복 제거

--- src/app/digging/page.tsx
+++ src/app/digging/page.tsx
@@ -62,8 +62,10 @@ export default function DiggingPage() {
   }, [inView, hasNextPage, fetchNextPage]);
 
   // 페이지별로 나뉜 배열( [[1~30], [31~60]] )을 하나의 배열로 flat 펴주기
-  const results = data?.pages.flat() || [];
-
+  const rawResults = data?.pages.flat() || [];
+  const results = rawResults.filter(
+    (item, index, self) => index === self.findIndex((t) => t.id === item.id), // ID가 처음 등장한 위치(findIndex)일 때만 살아남음
+  );
   return (
     <main className={styles.container}>
       {!isRelaxMode && (
